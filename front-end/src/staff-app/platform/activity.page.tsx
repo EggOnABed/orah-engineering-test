b87b54521b1b1dabfebcf0542aebd8507fd6e634
Sachin Dangi <dangisachin@gmail.com>
2022-10-18 15:26:29 +0530

    activity page data

--- front-end/src/staff-app/platform/activity.page.tsx
+++ front-end/src/staff-app/platform/activity.page.tsx
@@ -2,6 +2,9 @@ import React, { useEffect, useState } from "react"
 import styled from "styled-components"
 import { useApi } from "shared/hooks/use-api"
 import { Activity } from "shared/models/activity"
+import { RollStateList } from "staff-app/components/roll-state/roll-state-list.component"
+import * as moment from 'moment'
+import { Roll } from "shared/models/roll"
 
 export const ActivityPage: React.FC = () => {
   const [getActivities, activityData] = useApi<{ activity: Activity[] }>({ url: "get-activities" })
@@ -18,17 +21,26 @@ export const ActivityPage: React.FC = () => {
     getActivities()
   },[getActivities])
 
+  function reducer(source:any, reduceBy:string){
+    return source.reduce((total:any,current:any)=>{ return total + (current.roll_state === reduceBy ? 1 : 0) },0)
+  }
+
   return <S.Container>
     {
       activity?.length! > 0 ? <>
         {
           activity?.map(item=>{
-            return <div>
-              <span>Time</span>
-              <div>
-                
-              </div>
-            </div>
+            return <S.ActivityList key={ JSON.stringify(item.date) }>
+              <S.Time>{ moment(item.date).format('LLL') }</S.Time>
+              <RollStateList
+                stateList={[
+                  { type: "all", count : item.entity.student_roll_states.length },
+                  { type: "present", count: reducer(item.entity.student_roll_states, 'present') },
+                  { type: "late", count: reducer(item.entity.student_roll_states, 'late') },
+                  { type: "absent", count: reducer(item.entity.student_roll_states, 'absent') },
+                ]}
+              />
+            </S.ActivityList>
           })
         }
       </> : <S.NoActivity>No Activity as yet. Looks like holidays.</S.NoActivity>
@@ -41,7 +53,7 @@ const S = {
     display: flex;
     flex-direction: column;
     width: 50vw;
-    margin: 5vh auto 0;
+    margin: 10vh auto 0;
   `,
   NoActivity: styled.span`
     display: flex;
@@ -50,5 +62,23 @@ const S = {
     font-size: 35px;
     font-weight: 200;
     word-spacing: 5px;
+  `,
+  ActivityList: styled.div`
+    height: 100px;
+    font-size: 20px;
+    display: -webkit-box;
+    display: -webkit-flex;
+    display: -ms-flexbox;
+    display: flex;
+    border-bottom: solid 0.5px;
+    padding: 10px;
+    cursor: pointer;
+    &:hover{
+      background-color: gainsboro;
+    }
+  `,
+  Time: styled.span`
+    margin: auto;
+    margin-left: 0;
   `
 }
